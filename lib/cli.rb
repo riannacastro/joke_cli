30ac9987635eebae0d118827a1c2782472508f90
Rianna <riannaxcastro@gmail.com>
2021-04-05 22:01:22 -1000

    add methods to cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,27 @@
 class CLI
 # binding.pry
+
+    def start
+        puts "Hello! We are the Jokesters! What is your name?" 
+        input = user_input
+        greeting(input)
+
+    end
+
+    def user_input
+        gets.strip
+    end
+
+    def greeting(name)
+        puts "Nice to meet you, #{name}! Would you like to hear a joke?"
+
+    end
+
+    def choices
+    
+    end
+
+
+
+
 end
--- lib/joke.rb
+++ lib/joke.rb
@@ -1,6 +1,26 @@
 class Joke
 
-    attr_accessor :setup :delivery
+    attr_accessor :setup, :delivery
+
+    @@all = []
+
+    def initialize(setup, delivery)
+        @setup = setup
+        @delivery = delivery
+        save
+    
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+# binding.pry
 
 
 end
