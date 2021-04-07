120a1b144aec92e10a2b886b0a218ce459064522
Rianna <riannaxcastro@gmail.com>
2021-04-06 16:13:08 -1000

    added method

--- lib/api.rb
+++ lib/api.rb
@@ -1,4 +1,8 @@
 class API 
-# def self.get_data
-#     RestClient.get('')
- end
+    
+    def self.get_data
+       response = RestClient.get("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single")
+        binding.pry
+        jokes_array = JSON.parse(response)
+    end
+end
