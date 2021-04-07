51dea9b67ba5b99966f237f7ec49fd074415e870
Rianna <riannaxcastro@gmail.com>
2021-04-06 15:45:27 -1000

    added methods

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,13 +13,40 @@ class CLI
     end
 
     def greeting(name)
-        puts "Nice to meet you, #{name}! Would you like to hear a joke?"
-
+        puts "Nice to meet you, #{name}! Would you like to hear a joke? Enter \"yes\" to see list or \"exit\" to quit."
+        choices
     end
 
-    def choices
+     def choices
+        selection = user_input
+        if selection == "yes"
+            #print jokes
+            print_jokes
+            choices
+        elsif selection == "exit"
+            puts goodbye
+
+        else
+            invalid 
+        end
+     end
+
+     def goodbye
+        puts "Thank you for viewing the Jokesters! See you again!"
+     end
+
+     def invalid
+        puts "sorry... please choose a valid response."
+        choices
+     end
+
+     def print_jokes
+        jokes = ["Christmas", "Programming", "Pun", "Misc"]
+        jokes.each.with_index(1) do |joke, index|
+            puts "#{index}. #{joke}"
+        end
+    end
     
-    end
 
 
 
