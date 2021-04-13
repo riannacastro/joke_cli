1e6b68326ca1d882817ba19059ca7225ccf21ab2
Rianna Castro <riannaxcastro@gmail.com>
2021-04-12 14:12:54 -1000

    added notes

--- lib/cli.rb
+++ lib/cli.rb
@@ -42,9 +42,9 @@ class CLI
 
      def print_jokes
         Joke.all.each.with_index(1) do |joke, index|
-            puts "#{index}. #{joke[:category]}" # want to print category of jokes here
+            puts "#{index}. #{joke[:category]}" # want to print category of jokes here???
         end
-        binding.pry
+        #binding.pry
         select_joke
     end
 
@@ -55,7 +55,7 @@ class CLI
     end
 
     def joke_from_topic(joke)
-        puts "#{joke}"
+        puts "#{joke[:category]}" #puts category here?
         choices
     end
 
