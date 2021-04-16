f9639e496f9a6ce8a523bbd67ed180b8039a6f37
Rianna Castro <riannaxcastro@gmail.com>
2021-04-16 13:32:00 -1000

    erased notes in api

--- lib/api.rb
+++ lib/api.rb
@@ -3,11 +3,11 @@ class API
      def self.get_data
         response = RestClient.get("https://v2.jokeapi.dev/joke/Programming,Pun?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart&amount=10")
          jokes_array = JSON.parse(response)["jokes"]   
-         # binding.pry
+        
         jokes_array.each do |joke|
-            #binding.pry
+            
             Joke.new(joke)
         end
-        #binding.pry
+        
      end
 end
